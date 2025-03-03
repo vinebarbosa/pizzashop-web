de99d2cb452e6bf3fa20e88afecbfb476061624f
Vinícios Barbosa <viniciosbarbosa.dev@gmail.com>
2025-03-03 11:01:43 -0300

    chore: setup test environment

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import path from "path"
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
+
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,5 +10,10 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  test: {
+    globals: true,
+    setupFiles: ['./test/setup.ts'],
+    environment: 'happy-dom'
   },
 })
