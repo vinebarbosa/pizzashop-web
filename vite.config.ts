d661d28f2e5054d7b48d1d261feff3f3d3b3a00c
Vinícios Barbosa <viniciosbarbosa.dev@gmail.com>
2025-03-14 21:09:07 -0300

    fix: tests configuration

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
   },
   test: {
     globals: true,
-    setupFiles: ['./test/setup.ts'],
+    setupFiles: ['./tests/setup.ts'],
     environment: 'happy-dom'
   },
 })
